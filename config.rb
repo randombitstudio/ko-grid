d0c1fc81810d277a9f35fc69d031f8768a161ccc
bb <bb@randombitstudio.com>
2013-10-29 08:53:42 +0000

    Moving dev commit.

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "dist"
+generated_images_dir = "images/sprites"
+
+relative_assets = true
+debug_info = false
